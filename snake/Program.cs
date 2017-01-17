fdb18ab506a94de18b2e6a14911f013d731938b0
nicktriada <nicktriada@mail.ru>
2017-01-16 22:31:23 -0800

    les3

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,20 +10,19 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("HELLO");
 
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
-            Console.ReadLine();
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-        }
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
